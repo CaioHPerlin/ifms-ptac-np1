609be46b6ba5c3307ccecc8f42545af27650b2cf
Caio H Perlin <caiohperlin@gmail.com>
2023-09-19 01:52:11 -0400

    refactor: componentize navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
-import Link from 'next/link'
 import './globals.css'
-import Button from './components/Button'
+import Navbar from './components/Navbar'
 
 export const metadata = {
   title: 'JWT + Toast',
@@ -11,12 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='pt-br'>
       <body className='bg-pink-50 grid grid-cols-1 justify-items-center'>
-        <nav className='flex bg-white w-screen shadow-md align-middle p-5'>
-          <Link href={'/'}><Button secondary>Login</Button></Link>
-          <Link href={'/pages/dashboard/'}><Button secondary>Dashboard</Button></Link>
-          <Link href={'/pages/dashboard/register'}><Button secondary>Register</Button></Link>
-          <Link href={'/pages/dashboard/alter'}><Button secondary>Alter</Button></Link>
-        </nav>
+        <Navbar/>
         {children}
       </body>
     </html>
