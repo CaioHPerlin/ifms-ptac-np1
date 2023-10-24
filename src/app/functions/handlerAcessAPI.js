c9442ce56aa24265002b73f7357d7129ec184989
Caio H Perlin <caiohperlin@gmail.com>
2023-10-23 22:56:55 -0400

    feat: fetching users from API

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,41 +1,24 @@
 'use server'
 
-const usersDb = [
-    {
-        name: 'John Doe',
-        email: 'johndoe@yahoo.com',
-        password: 'foo',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    },
-    {
-        name: 'Caio Hygino',
-        email: 'caiocaio@gmail.com',
-        password: 'caio',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    },
-    {
-        name: 'Vito Vito',
-        email: 'vito@yahoo.com',
-        password: 'filhodojales',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    },{
-        name: 'João Santos',
-        email: 'joao.santos@gmail.com',
-        password: '123',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    }
-];
+const databaseUrl = 'https://ifms-ptac-2023-2.vercel.app'
 
-const getUserAuthenticated = (user) => {
-    let userAuth = {};
-    for(let i = 0; i < usersDb.length; i++){
-        if(user.email == usersDb[i].email && user.password == usersDb[i].password){
-            userAuth = usersDb[i];
-        }
-    }
+const getUserAuthenticated = async (user) => {
+    const response = await fetch(databaseUrl + "/user/authenticated",
+    {
+        cache:"no-cache",
+        method:"POST",
+        headers:{"Content-Type": "Application/json"},
+        body: JSON.stringify(user)
+    });
+
+    const userAuth = await response.json();
     return userAuth;
 }
 
-const getUsers = () => usersDb;
+const getUsers = async () => {
+    const response = await fetch(databaseUrl + "/users", { cache:"no-cache" });
+    const users = await response.json();
+    return users;
+}
 
 export { getUsers, getUserAuthenticated };
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,14 +1,12 @@
 import UserList from '@/app/components/UserList';
 import LogoutButton from '@/app/components/LogoutButton';
-import Link from 'next/link';
 import { getUsers } from '@/app/functions/handlerAcessAPI';
 import { Suspense } from 'react';
 import Block from '@/app/components/Block';
-import Button from '@/app/components/Button';
 
 export default async function Dashboard() {
    
-    const users = getUsers();
+    const users = await getUsers();
 
     return (
         <Block title={'Dashboard'}>
